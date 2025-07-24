d443e1ff1832f2a1cb83920a49db9fafa17fd12e
Turbobot <turbobot@vercel.com>
2025-07-24 17:58:04 +0530

    feat(create-turbo): apply official-starter transform

--- client/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Landing from "../screens/Landing";
-
-export default function Home() {
-  return (
-    <div className="h-screen bg-sky-950">
-      <Landing />
-    </div>
-  );
-}
--- client/components/landing/Button.tsx
+++
@@ -1,9 +0,0 @@
-const Button = () => {
-  return (
-    <div>
-      <button className="btn btn-outline btn-accent">Accent</button>
-    </div>
-  );
-};
-
-export default Button;
